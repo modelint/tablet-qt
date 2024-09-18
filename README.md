2c263e1bb71440703dea15ba1572c608c13d12bf
Leon Starr <leon_starr@modelint.com>
2024-09-18 12:31:55 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tablet
+
+Preliminary Read-me template, more info to be filled in soon
+
+### Why you need this
+
+You want to draw simple 2D to support MBSE schematic like drawings
+
+### Installation
+
+Create or use a python 3.12+ environment. Then
+
+% pip install tabletqt
+
+#### From your python script
+
+You need this import statement at a minimum:
+
+    <TBD>
+
+#### From the command line
+
+This is not the intended usage scenario, but may be helpful for testing or exploration. Since scrall may generate
+some diagnostic info you may want to create a fresh working directory and cd into it first. From there...
+
+    % tabletqt
+
+You should also see a file named `tabletqt.log`
---
+++ src/tabletlib/__main__.py
@@ -0,0 +1,73 @@
+"""
+__main__.py
+"""
+
+import logging
+import logging.config
+import sys
+import argparse
+from tabletlib import version
+from pathlib import Path
+# from tabletlib.etchasketch import EtchaSketch
+
+_logpath = Path("tabletlib.log")
+app_name = "Tablet"
+
+def get_logger():
+    """Initiate the logger"""
+    log_conf_path = Path(__file__).parent / 'log.conf'  # Logging configuration is in this file
+    logging.config.fileConfig(fname=log_conf_path, disable_existing_loggers=False)
+    return logging.getLogger(__name__)  # Create a logger for this module
+
+# Configure the expected parameters and actions for the argparse module
+def parse(cl_input):
+    """
+    The command line interface is for diagnostic purposes
+
+    :param cl_input:
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Tabletx 2D draw interface to Cairo')
+    parser.add_argument('-COLORS', '--colors', action='store_true',
+                        help='Show the list of background color names')
+    parser.add_argument('-T', '--test', action='store_true',
+                        help='Run some test features')
+    parser.add_argument('-D', '--debug', action='store_true',
+                        help='Debug mode'),
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='Print the current version')
+    return parser.parse_args(cl_input)
+
+
+def main():
+    # Start logging
+    logger = get_logger()
+    logger.info(f'{app_name} version: {version}')
+    logger.info('-----------------\n')
+
+    # Parse the command line args
+    args = parse(sys.argv[1:])
+
+    if args.version:
+        # Just print the version and quit
+        print(f'{app_name} version: {version}')
+        sys.exit(0)
+
+    if args.test:
+        from tabletlib.styledb import StyleDB
+        StyleDB.load_config_files()
+
+    if args.colors:
+        # Just print the database colors and quit
+        from tabletlib.styledb import StyleDB
+        StyleDB.report_colors()
+
+    # EtchaSketch.draw_stuff()
+    # EtchaSketch.compare2()
+
+    logger.info("No problemo")  # We didn't die on an exception
+    print("\nNo problemo")
+
+
+if __name__ == "__main__":
+    main()
