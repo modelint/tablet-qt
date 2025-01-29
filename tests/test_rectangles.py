cd832cb61c77c3a02285d552200ae2c1f290b664
Leon Starr <leon_starr@modelint.com>
2025-01-28 17:49:21 -0800

    all rectangle tests run ok

--- tests/test_rectangles.py
+++ tests/test_rectangles.py
@@ -16,6 +16,20 @@ def test_class_compartment():
     output_path = Path(f"output/{function_name}.pdf")
 
     test_tablet = Tablet(size=A4, output_file=output_path, drawing_type=dtype,
+                         presentation="default", layer="diagram", show_window=False, background_color='blue steel')
+    dlayer = test_tablet.layers['diagram']
+
+    r = RectangleSE.add(layer=dlayer, asset="class name compartment", lower_left=Position(100, 100),
+                        size=Rect_Size(height=27, width=253))
+    test_tablet.render()
+    assert True
+
+def test_bp_class_compartment():
+    dtype = "Starr class diagram"
+    function_name = inspect.currentframe().f_code.co_name
+    output_path = Path(f"output/{function_name}.pdf")
+
+    test_tablet = Tablet(size=A4, output_file=output_path, drawing_type=dtype,
                          presentation="blueprint", layer="diagram", show_window=False, background_color='blueprint')
     dlayer = test_tablet.layers['diagram']
 
@@ -25,6 +39,19 @@ def test_class_compartment():
     assert True
 
 def test_state_compartment():
+    dtype = "xUML state machine diagram"
+    function_name = inspect.currentframe().f_code.co_name
+    output_path = Path(f"output/{function_name}.pdf")
+
+    test_tablet = Tablet(size=A4, output_file=output_path, drawing_type=dtype,
+                         presentation="default", layer="diagram", show_window=False, background_color='autumn')
+    dlayer = test_tablet.layers['diagram']
+
+    r = RectangleSE.add(layer=dlayer, asset="state name only compartment", lower_left=Position(100, 100),
+                        size=Rect_Size(height=27, width=253))
+    test_tablet.render()
+    assert True
+def test_bp_state_compartment():
     dtype = "xUML state machine diagram"
     function_name = inspect.currentframe().f_code.co_name
     output_path = Path(f"output/{function_name}.pdf")
